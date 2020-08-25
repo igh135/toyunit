ecadc4d487c36c8a7c6edcbf96f9d23057c85b59
igh135 <61082748+igh135@users.noreply.github.com>
2020-08-25 20:10:57 +0900

    Update settings.py

--- toy_project/settings.py
+++ toy_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '!e=0gqg*&4@@x^y*^9lgyu7%zd-9qmz@jwm99mou9uzfid4z^+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
 
-ALLOWED_HOSTS = [54.180.61.126]
+ALLOWED_HOSTS = []
 
 # Application definition
 
