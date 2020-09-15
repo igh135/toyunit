8ab16e6900ce8e5709b089420846be45cca39fd1
igh135 <61082748+igh135@users.noreply.github.com>
2020-09-15 21:46:44 +0900

    Update settings.py

--- toy_project/settings.py
+++ toy_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!e=0gqg*&4@@x^y*^9lgyu7%zd-9qmz@jwm99mou9uzfid4z^+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 
 ALLOWED_HOSTS = ["54.180.61.126","127.0.0.1"]
 
