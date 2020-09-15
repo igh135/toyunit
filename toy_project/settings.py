42ab62449cedde148565434dda3802fe33e87403
igh135 <61082748+igh135@users.noreply.github.com>
2020-09-15 21:20:57 +0900

    Update settings.py

--- toy_project/settings.py
+++ toy_project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'toyproject',
         'USER': 'dbmasteruser',
         'PASSWORD': 'han000719*',
-        'HOST': '172.26.4.112',
+        'HOST': '54.180.61.126',
         'PORT': '3306',
     }
 }
